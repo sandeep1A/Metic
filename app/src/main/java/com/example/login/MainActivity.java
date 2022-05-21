e3ac312ea07f9f5ed5e659c143e48b5dcff7d00d
Sandeep <sandeepp502@gmail.com>
2022-05-21 20:17:03 +0530

    Initial commit

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,6 +1,7 @@
 
 package com.example.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,8 +9,14 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         enternumber = findViewById(R.id.input_number);
         getotpbutton = findViewById(R.id.button_otp);
+        final ProgressBar progressBar=findViewById(R.id.progressbar_sendingotp);
 
 
            getotpbutton.setOnClickListener(new View.OnClickListener() {
@@ -29,9 +37,46 @@ public class MainActivity extends AppCompatActivity {
                public void onClick(View view) {
                    if(!enternumber.getText().toString().trim().isEmpty()){
                        if((enternumber.getText().toString().trim()).length() == 10) {
-                           Intent intent = new Intent(getApplicationContext(), verifyotp.class);
-                           intent.putExtra("mobile", enternumber.getText().toString());
+                           progressBar.setVisibility(View.VISIBLE);
+                           getotpbutton.setVisibility(View.INVISIBLE);
+                           PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                                   "+91" + enternumber.getText().toString(),
+                                   60,
+                           TimeUnit.SECONDS,
+                                   MainActivity.this,
+                                   new PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+
+                                       @Override
+                                       public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                           progressBar.setVisibility(View.GONE);
+                                           getotpbutton.setVisibility(View.INVISIBLE);
+
+                                       }
+
+                                       @Override
+                                       public void onVerificationFailed(@NonNull FirebaseException e) {
+                                           progressBar.setVisibility(View.GONE);
+                                           getotpbutton.setVisibility(View.VISIBLE);
+                                           Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                                       }
+
+                                       @Override
+                                       public void onCodeSent(@NonNull String backendotp, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                           progressBar.setVisibility(View.GONE);
+                                           getotpbutton.setVisibility(View.VISIBLE);
+                                           Intent intent = new Intent(getApplicationContext(), verifyotp.class);
+                                           intent.putExtra("mobile", enternumber.getText().toString());
+                                           intent.putExtra("backendotp",backendotp);
+                                           startActivity(intent);
+                                       }
+                                   }
+
+                       );
+                           Intent intent = new Intent(getApplicationContext(),verifyotp.class);
+                           intent.putExtra("mobile",enternumber.getText().toString());
                            startActivity(intent);
+
                        }else{
                            Toast.makeText(MainActivity.this,"Please enter correct number",Toast.LENGTH_SHORT).show();
                        }
